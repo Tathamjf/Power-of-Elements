2df0956536db6ce13d6e05d428361cb11a5a647a
Agatha Moreira de Jesus Freitas <56998304803@info.local>
2025-04-08 17:16:04 -0300

    feat: Movimentação em terceira pessoa

--- Assets/Scripts/Movimento_Player.cs
+++ Assets/Scripts/Movimento_Player.cs
@@ -2,37 +2,43 @@ using UnityEngine;
 
 public class Movimento_Player : MonoBehaviour
 {
-
+    private Transform myCamera;
     private CharacterController controller;
-    private Transform mycamera;
-
-
+    private Animator animator;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        controller = GetComponent<CharacterController>();
-        mycamera = Camera.main.transform;
-       
+    {              
+       myCamera = Camera.main.transform;
+      controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
+        float vertiacal = Input.GetAxis("Vertical");
 
-        Vector3 movimento = new Vector3 (horizontal, 0, vertical);
-        movimento = mycamera.TransformDirection(movimento);
+        Vector3 movimento = new Vector3(horizontal, 0, vertiacal);
+
+        movimento = myCamera.TransformDirection(movimento);
         movimento.y = 0;
 
-        controller.Move(movimento * Time.deltaTime * 3);
+        controller.Move(movimento * Time.deltaTime * 5);
+
         controller.Move(new Vector3(0, -9.81f, 0) * Time.deltaTime);
 
         if (movimento != Vector3.zero)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movimento), Time.deltaTime * 10);
-        } 
+        }
+
+        animator.SetBool("IsWalking", movimento != Vector3.zero);
+
 
     }
 }
